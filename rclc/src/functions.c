95e6ea4151379f959bc684dfd1155544d77ad42a
Loïc Dauphin <astralien3000@yahoo.fr>
2017-11-09 11:19:26 +0100

    some debug + improve returns

--- rclc/src/functions.c
+++ rclc/src/functions.c
@@ -48,11 +48,11 @@ struct rclc_node_t
 rclc_ret_t
 rclc_init(int argc, char ** argv)
 {
-  rcl_ret_t ret = rcl_init(argc, argv, rcl_get_default_allocator());
-  if(ret != RCL_RET_OK) {
+  rcl_ret_t rc = rcl_init(argc, argv, rcl_get_default_allocator());
+  if(rc != RCL_RET_OK) {
     PRINT_RCL_ERROR(rclc_init, rcl_init);
   }
-  return ret;
+  return rc;
 }
 
 bool
@@ -134,19 +134,20 @@ rclc_spin_node(rclc_node_t * node)
     for (size_t i = 0; i < wait_set.size_of_subscriptions; ++i) {
       if (wait_set.subscriptions[i]) {
         char msg[64];
+
         rc = rcl_take(wait_set.subscriptions[i], (void*)&msg, NULL);
         if(rc != RCL_RET_OK) {
           PRINT_RCL_ERROR(rclc_spin_node, rcl_take);
           _rclc_spin_node_exit(&wait_set);
           return;
         }
-        else {
-          for (size_t i = 0; i < node->subs_s; ++i) {
-            if(&node->subs[i]->rcl_subscription == wait_set.subscriptions[i]) {
-              node->subs[i]->user_callback((const void*)&msg);
-            }
+
+        for (size_t j = 0; j < node->subs_s; ++j) {
+          if(&node->subs[j]->rcl_subscription == wait_set.subscriptions[i]) {
+            node->subs[j]->user_callback((const void*)&msg);
           }
         }
+
       }
     }
   }
@@ -157,27 +158,30 @@ rclc_spin_node(rclc_node_t * node)
 rclc_node_t *
 rclc_create_node(const char * name, const char * namespace_)
 {
-  rclc_node_t* ret = ALLOCATE(sizeof(rclc_node_t));
-  ret->rcl_node = rcl_get_zero_initialized_node();
-  ret->subs = NULL;
-  ret->subs_s = 0;
+  rclc_node_t* rclc_node = ALLOCATE(sizeof(rclc_node_t));
+  rclc_node->rcl_node = rcl_get_zero_initialized_node();
+  rclc_node->subs = NULL;
+  rclc_node->subs_s = 0;
   
   rcl_node_options_t node_ops = rcl_node_get_default_options();
-  rcl_ret_t rc = rcl_node_init(&ret->rcl_node, name, namespace_, &node_ops);
+  rcl_ret_t rc = rcl_node_init(&rclc_node->rcl_node, name, namespace_, &node_ops);
   if(rc != RCL_RET_OK) {
     PRINT_RCL_ERROR(rclc_create_node, rcl_node_init);
     return NULL;
   }
   
-  return ret;
+  return rclc_node;
 }
 
 rclc_ret_t
 rclc_destroy_node(rclc_node_t * node)
 {
-  rcl_ret_t ret = rcl_node_fini(&node->rcl_node);
+  rcl_ret_t rc = rcl_node_fini(&node->rcl_node);
+  if(rc != RCL_RET_OK) {
+    PRINT_RCL_ERROR(rclc_destroy_node, rcl_node_fini);
+  }
   DEALLOCATE(node);
-  return ret;
+  return rc;
 }
 
 rclc_publisher_t *
@@ -189,18 +193,18 @@ rclc_create_publisher(
 {
   (void)queue_size;
   
-  rclc_publisher_t* ret = ALLOCATE(sizeof(rclc_publisher_t));
-  ret->rcl_publisher = rcl_get_zero_initialized_publisher();
-  ret->node = node;
+  rclc_publisher_t* rclc_publisher = ALLOCATE(sizeof(rclc_publisher_t));
+  rclc_publisher->rcl_publisher = rcl_get_zero_initialized_publisher();
+  rclc_publisher->node = node;
 
   rcl_publisher_options_t pub_opt = rcl_publisher_get_default_options();
-  rcl_ret_t rc = rcl_publisher_init(&ret->rcl_publisher, &node->rcl_node, type_support, topic_name, &pub_opt);
+  rcl_ret_t rc = rcl_publisher_init(&rclc_publisher->rcl_publisher, &node->rcl_node, type_support, topic_name, &pub_opt);
   if(rc != RCL_RET_OK) {
     PRINT_RCL_ERROR(rclc_create_publisher, rcl_publisher_init);
     return NULL;
   }
 
-  return ret;
+  return rclc_publisher;
 }
 
 rclc_ret_t
@@ -211,12 +215,17 @@ rclc_destroy_publisher(rclc_publisher_t * publisher)
     PRINT_RCL_ERROR(rclc_destroy_publisher, rcl_publisher_fini);
   }
   DEALLOCATE(publisher);
-  return RCL_RET_OK;
+  return rc;
 }
 
 rclc_ret_t
 rclc_publish(const rclc_publisher_t * publisher, const void * ros_message)
 {
+  if(publisher == NULL) {
+    fprintf(stderr, "[rclc_publish] null pointer to publisher");
+    return RCL_RET_INVALID_ARGUMENT;
+  }
+
   return rcl_publish(&publisher->rcl_publisher, ros_message);
 }
 
@@ -232,13 +241,13 @@ rclc_create_subscription(
   (void)queue_size;
   (void)ignore_local_publications;
 
-  rclc_subscription_t* ret = ALLOCATE(sizeof(rclc_subscription_t));
-  ret->rcl_subscription = rcl_get_zero_initialized_subscription();
-  ret->user_callback = callback;
-  ret->node = node;
+  rclc_subscription_t* rclc_subscription = ALLOCATE(sizeof(rclc_subscription_t));
+  rclc_subscription->rcl_subscription = rcl_get_zero_initialized_subscription();
+  rclc_subscription->user_callback = callback;
+  rclc_subscription->node = node;
 
   rcl_subscription_options_t subscription_ops = rcl_subscription_get_default_options();
-  rcl_ret_t rc = rcl_subscription_init(&ret->rcl_subscription, &node->rcl_node, type_support, topic_name, &subscription_ops);
+  rcl_ret_t rc = rcl_subscription_init(&rclc_subscription->rcl_subscription, &node->rcl_node, type_support, topic_name, &subscription_ops);
   if(rc != RCL_RET_OK) {
     PRINT_RCL_ERROR(rclc_create_subscription, rcl_subscription_init);
     return NULL;
@@ -249,18 +258,30 @@ rclc_create_subscription(
   }
   node->subs_s++;
   node->subs = REALLOCATE(node->subs, sizeof(rclc_subscription_t*)*node->subs_s);
-  node->subs[node->subs_s-1] = ret;
-  return ret;
+  node->subs[node->subs_s-1] = rclc_subscription;
+  return rclc_subscription;
 }
 
 rclc_ret_t
 rclc_destroy_subscription(rclc_subscription_t * subscription)
 {
-  // TODO : remove from subs list
+  bool found = false;
+  for(size_t i = 0 ; i < subscription->node->subs_s ; ++i) {
+    if(found) {
+      subscription->node->subs[i-1] = subscription->node->subs[i];
+    }
+    if(subscription == subscription->node->subs[i]) {
+      found = true;
+    }
+  }
+  if(found) {
+    subscription->node->subs_s--;
+  }
+
   rcl_ret_t rc = rcl_subscription_fini(&subscription->rcl_subscription, &subscription->node->rcl_node);
   if(rc != RCL_RET_OK) {
     PRINT_RCL_ERROR(rclc_destroy_subscription, rcl_subscription_fini);
   }
   DEALLOCATE(subscription);
-  return RCL_RET_OK;
+  return rc;
 }
