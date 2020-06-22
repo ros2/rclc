34398c433e63a66133e5cc874e3304f13b26be5b
Arne Nordmann <arne.nordmann@de.bosch.com>
2020-06-22 12:10:18 +0200

    Further tests: lifeycycle node transitions
    Signed-off-by: Arne Nordmann <arne.nordmann@de.bosch.com>

--- rclc_lifecycle/test/test_lifecycle.cpp
+++ rclc_lifecycle/test/test_lifecycle.cpp
@@ -21,6 +21,9 @@
 extern "C"
 {
 #include "rclc_lifecycle/rclc_lifecycle.h"
+
+#include <lifecycle_msgs/msg/state.h>
+#include <lifecycle_msgs/msg/transition.h>
 }
 
 TEST(TestRclcLifecycle, lifecycle_node) {
@@ -48,4 +51,66 @@ TEST(TestRclcLifecycle, lifecycle_node) {
   EXPECT_EQ(
     RCL_RET_OK,
     rcl_lifecycle_state_machine_is_initialized(lifecycle_node.state_machine));
+}
+
+TEST(TestRclcLifecycle, lifecycle_node_transitions) {
+  rcl_context_t context = rcl_get_zero_initialized_context();
+  rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+
+  rcl_init_options_init(&init_options, allocator);
+  rcl_init(0, nullptr, &init_options, &context);
+
+  rcl_node_t my_node = rcl_get_zero_initialized_node();
+  rcl_node_options_t node_ops = rcl_node_get_default_options();
+  rcl_node_init(&my_node, "lifecycle_node", "rclc", &context, &node_ops);
+
+  rclc_lifecycle_node_t lifecycle_node;
+  rcl_lifecycle_state_machine_t state_machine_ = rcl_lifecycle_get_zero_initialized_state_machine();
+
+  rcl_ret_t res = rclc_make_node_a_lifecycle_node(
+    &lifecycle_node,
+    &my_node,
+    &state_machine_,
+    &node_ops);
+
+  // configure
+  res = rclc_lifecycle_change_state(
+    &lifecycle_node,
+    lifecycle_msgs__msg__Transition__TRANSITION_CONFIGURE,
+    true);
+  EXPECT_EQ(RCL_RET_OK, res);
+  EXPECT_EQ(
+    lifecycle_msgs__msg__State__PRIMARY_STATE_INACTIVE,
+    lifecycle_node.state_machine->current_state->id);
+
+  // activate
+  res = rclc_lifecycle_change_state(
+    &lifecycle_node,
+    lifecycle_msgs__msg__Transition__TRANSITION_ACTIVATE,
+    true);
+  EXPECT_EQ(RCL_RET_OK, res);
+  EXPECT_EQ(
+    lifecycle_msgs__msg__State__PRIMARY_STATE_ACTIVE,
+    lifecycle_node.state_machine->current_state->id);
+
+  // deactivate
+  res = rclc_lifecycle_change_state(
+    &lifecycle_node,
+    lifecycle_msgs__msg__Transition__TRANSITION_DEACTIVATE,
+    true);
+  EXPECT_EQ(RCL_RET_OK, res);
+  EXPECT_EQ(
+    lifecycle_msgs__msg__State__PRIMARY_STATE_INACTIVE,
+    lifecycle_node.state_machine->current_state->id);
+
+  // cleanup
+  res = rclc_lifecycle_change_state(
+    &lifecycle_node,
+    lifecycle_msgs__msg__Transition__TRANSITION_CLEANUP,
+    true);
+  EXPECT_EQ(RCL_RET_OK, res);
+  EXPECT_EQ(
+    lifecycle_msgs__msg__State__PRIMARY_STATE_UNCONFIGURED,
+    lifecycle_node.state_machine->current_state->id);
 }
