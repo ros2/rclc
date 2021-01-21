bf153bbe94693498eaa0e6e733afbb7f059cc6df
Arne Nordmann <arne.nordmann@de.bosch.com>
2021-01-21 14:47:09 +0100

    Macros for checking rcl return code
    Introduces two macros to check and handle the rcl return code.
    
    https://github.com/ros2/rclc/issues/33
    
    Signed-off-by: Arne Nordmann <arne.nordmann@de.bosch.com>

--- rclc/include/rclc/types.h
+++ rclc/include/rclc/types.h
@@ -64,6 +64,14 @@ typedef struct
 
 #define RCLC_UNUSED(x) (void)x
 
+/**
+ * macros to check rcl return code
+ */
+#define RCCHECK(fn) {rcl_ret_t temp_rc = fn; if ((temp_rc != RCL_RET_OK)) {printf( \
+        "Failed status on line %d: %d. Aborting.\n", __LINE__, (int)temp_rc); return 1;}}
+#define RCSOFTCHECK(fn) {rcl_ret_t temp_rc = fn; if ((temp_rc != RCL_RET_OK)) {printf( \
+        "Failed status on line %d: %d. Continuing.\n", __LINE__, (int)temp_rc);}}
+
 #if __cplusplus
 }
 #endif
