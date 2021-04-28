0a706d179205319cfbee3d8ba385227fed3d2207
Pablo Garrido <pablogs9@gmail.com>
2021-04-28 10:33:12 +0200

    Add parameter server first version
    Update with cmake parameter configuration
    
    Update with parameter event publisher
    
    Update with message memory management
    
    Add user callbacks for changed parameters
    
    Use only All parameters callback
    
    Initial refactor
    
    
    First refactor
    
    
    New on change API
    
    
    Update
    
    
    Fix copyright banner
    
    
    Refactor API
    
    
    Update static memory approach
    
    
    Update
    
    
    Free mem
    
    
    Refactor
    
    
    Add init with options
    
    
    Initial tests
    
    
    Dynamic memory
    
    
    Uncrustified
    
    
    Uncris
    
    
    Add documentation
    
    
    Add config
    
    
    Add rclc_parameter to action
    
    
    Typo
    
    
    Add describe
    
    
    Add callback test
    
    
    Remove config
    
    
    Update rclc_examples
    
    
    Update examples
    
    Signed-off-by: Jan Staschulat <jan.staschulat@de.bosch.com>

---
+++ rclc_examples/src/example_parameter_server.c
@@ -0,0 +1,113 @@
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <rclc_parameter/rclc_parameter.h>
+
+rclc_parameter_server_t param_server;
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+    (void) timer;
+    (void) last_call_time;
+
+	int value;
+    rclc_parameter_get(&param_server, "param2", &value);
+    value++;
+    rclc_parameter_set(&param_server, "param2", (int) value);
+}
+
+void on_parameter_changed(Parameter * param)
+{
+    printf("Parameter %s modified.", param->name.data);
+    switch (param->value.type)
+    {
+    case RCLC_PARAMETER_BOOL:
+        printf(" New value (bool) %d", param->value.bool_value);
+        break;
+    case RCLC_PARAMETER_INT:
+        printf(" New value (int) %ld", param->value.integer_value);
+        break;
+    case RCLC_PARAMETER_DOUBLE:
+        printf(" New value (double) %f", param->value.double_value);
+        break;
+    default:
+        break;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    rcl_ret_t rc;
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+
+    // Create init_options
+    rclc_support_t support;
+    rclc_support_init(&support, 0, NULL, &allocator);
+
+    // Create node
+    rcl_node_t node;
+    rclc_node_init_default(&node, "demo_param_node", "", &support);
+
+    // Create parameter service
+    rclc_parameter_server_init_default(&param_server, &node);
+
+    // create timer,
+	rcl_timer_t timer;
+	rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(1000),
+		timer_callback);
+
+    // Create executor
+    rclc_executor_t executor;
+    rclc_executor_init(&executor, &support.context, RCLC_PARAMETER_EXECUTOR_HANDLES_NUMBER + 1, &allocator);
+    rclc_executor_add_parameter_server(&executor, &param_server, on_parameter_changed);
+	rclc_executor_add_timer(&executor, &timer);
+
+    // Add parameters
+    rclc_add_parameter(&param_server, "param1", RCLC_PARAMETER_BOOL);
+    rclc_add_parameter(&param_server, "param2", RCLC_PARAMETER_INT);
+    rclc_add_parameter(&param_server, "param3", RCLC_PARAMETER_DOUBLE);
+
+    bool param1;
+    int param2;
+    double param3;
+
+    // TODO: We need to choose between this API:
+    rclc_parameter_set(&param_server, "param1", (bool) false);
+    rclc_parameter_set(&param_server, "param2", (int) 10);
+    rclc_parameter_set(&param_server, "param3", (double) 0.01);
+
+    rclc_parameter_get(&param_server, "param1", &param1);
+    rclc_parameter_get(&param_server, "param2", &param2);
+    rclc_parameter_get(&param_server, "param3", &param3);
+
+    // Or this API
+    rclc_parameter_set_bool(&param_server, "param1", false);
+    rclc_parameter_set_int(&param_server, "param2", 10);
+    rclc_parameter_set_double(&param_server, "param3", 0.01);
+
+    rclc_parameter_get_bool(&param_server, "param1", &param1);
+    rclc_parameter_get_int(&param_server, "param2", &param2);
+    rclc_parameter_get_double(&param_server, "param3", &param3);
+
+    rclc_executor_spin(&executor);
+
+    // clean up
+    rc = rclc_executor_fini(&executor);
+    rc += rclc_parameter_server_fini(&param_server, &node);
+    rc += rcl_node_fini(&node);
+
+    if (rc != RCL_RET_OK) {
+        printf("Error while cleaning up!\n");
+        return -1;
+    }
+    return 0;
+}
---
+++ rclc_parameter/src/rclc_parameter/parameter_utils.c
@@ -0,0 +1,101 @@
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if __cplusplus
+extern "C"
+{
+#endif /* if __cplusplus */
+
+#include "rclc_parameter/parameter_utils.h"
+
+rcl_ret_t
+rclc_parameter_value_copy(
+  ParameterValue * dst,
+  const ParameterValue * src)
+{
+  RCL_CHECK_ARGUMENT_FOR_NULL(dst, RCL_RET_INVALID_ARGUMENT);
+  RCL_CHECK_ARGUMENT_FOR_NULL(src, RCL_RET_INVALID_ARGUMENT);
+
+  dst->type = src->type;
+
+  switch (src->type) {
+    case RCLC_PARAMETER_BOOL:
+      dst->bool_value = src->bool_value;
+      return RCL_RET_OK;
+    case RCLC_PARAMETER_INT:
+      dst->integer_value = src->integer_value;
+      return RCL_RET_OK;
+    case RCLC_PARAMETER_DOUBLE:
+      dst->double_value = src->double_value;
+      return RCL_RET_OK;
+    case RCLC_PARAMETER_NOT_SET:
+    default:
+      dst->type = RCLC_PARAMETER_NOT_SET;
+      return RCL_RET_ERROR;
+  }
+  return RCL_RET_ERROR;
+}
+
+rcl_ret_t
+rclc_parameter_copy(
+  Parameter * dst,
+  const Parameter * src)
+{
+  RCL_CHECK_ARGUMENT_FOR_NULL(dst, RCL_RET_INVALID_ARGUMENT);
+  RCL_CHECK_ARGUMENT_FOR_NULL(src, RCL_RET_INVALID_ARGUMENT);
+
+  rclc_parameter_set_string(&dst->name, src->name.data);
+  return rclc_parameter_value_copy(&dst->value, &src->value);
+}
+
+Parameter * rclc_parameter_search(
+  Parameter__Sequence * parameter_list,
+  const char * param_name)
+{
+  for (size_t i = 0; i < parameter_list->size; i++) {
+    if (!strcmp(param_name, parameter_list->data[i].name.data)) {
+      return &parameter_list->data[i];
+    }
+  }
+
+  return NULL;
+}
+
+bool rclc_parameter_set_string(
+  rosidl_runtime_c__String * str,
+  const char * value)
+{
+  return rosidl_runtime_c__String__assign(str, value);
+}
+
+void rclc_parameter_prepare_parameter_event(
+  ParameterEvent * event,
+  Parameter * parameter,
+  bool new)
+{
+  memset(&event->new_parameters, 0, sizeof(Parameter__Sequence));
+  memset(&event->changed_parameters, 0, sizeof(Parameter__Sequence));
+
+  Parameter__Sequence * seq = (new) ?
+    &event->new_parameters :
+    &event->changed_parameters;
+
+  seq->data = parameter;
+  seq->capacity = 1;
+  seq->size = 1;
+}
+
+#if __cplusplus
+}
+#endif /* if __cplusplus */
