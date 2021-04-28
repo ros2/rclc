a7d9fc573d228a9052414e7e813bcbe17a7f6b43
Pablo Garrido <pablogs9@gmail.com>
2021-04-28 08:43:12 +0200

    Add documentation

--- rclc_parameter/include/rclc_parameter/rclc_parameter.h
+++ rclc_parameter/include/rclc_parameter/rclc_parameter.h
@@ -39,6 +39,8 @@ extern "C"
 #include <rosidl_runtime_c/string_functions.h>
 #include <rosidl_runtime_c/primitives_sequence_functions.h>
 
+// Alias for rcl_interfaces types
+
 typedef struct rcl_interfaces__srv__GetParameters_Request GetParameters_Request;
 typedef struct rcl_interfaces__srv__GetParameters_Response GetParameters_Response;
 
@@ -57,10 +59,13 @@ typedef struct rcl_interfaces__msg__ParameterValue ParameterValue;
 typedef struct rcl_interfaces__msg__Parameter__Sequence Parameter__Sequence;
 typedef struct rcl_interfaces__msg__ParameterEvent ParameterEvent;
 
+// Number of RCLC executor handles required for a parameter server
 #define RCLC_PARAMETER_EXECUTOR_HANDLES_NUMBER 6
 
+// On parameter modified callback
 typedef void (* ModifiedParameter_Callback)(Parameter * param);
 
+// Allowed RCLC parameter types
 typedef enum rclc_parameter_type_t
 {
   RCLC_PARAMETER_NOT_SET = 0,
@@ -69,12 +74,14 @@ typedef enum rclc_parameter_type_t
   RCLC_PARAMETER_DOUBLE
 } rclc_parameter_type_t;
 
+// RCLC parameter server options
 typedef struct rclc_parameter_options_t
 {
   bool notify_changed_over_dds;
   size_t max_params;
 } rclc_parameter_options_t;
 
+// Container for RCLC parameter server
 typedef struct rclc_parameter_server_t
 {
   rcl_service_t get_service;
@@ -105,23 +112,106 @@ typedef struct rclc_parameter_server_t
   bool notify_changed_over_dds;
 } rclc_parameter_server_t;
 
+/**
+ *  Initializes a RCLC parameter server in a node with default configuration
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | Yes
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[inout] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] node related node
+ * \return `RCL_RET_OK` if the \p parameter_server was initialized successfully
+ */
+
 rcl_ret_t rclc_parameter_server_init_default(
   rclc_parameter_server_t * parameter_server,
   rcl_node_t * node);
+
+/**
+ *  Initializes a RCLC parameter server in a node with given configuration
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | Yes
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[inout] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] node related node
+ * \param[in] options rclc_parameter_options_t struct with parameter options
+ * \return `RCL_RET_OK` if the \p parameter_server was initialized successfully
+ */
 
 rcl_ret_t rclc_parameter_server_init_with_option(
   rclc_parameter_server_t * parameter_server,
   rcl_node_t * node,
   rclc_parameter_options_t * options);
 
+/**
+ *  Destroy a RCLC parameter server
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | Yes
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] node related node
+ * \return `RCL_RET_OK` if the \p parameter_server was destroyed successfully
+ */
+
 rcl_ret_t rclc_parameter_server_fini(
   rclc_parameter_server_t * parameter_server,
   rcl_node_t * node);
+
+/**
+ *  Adds a RCLC parameter server to an RCLC executor
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | Yes
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] executor RCLC executor
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] on_modification on parameter modification callback
+ * \return `RCL_RET_OK` if success
+ */
 
 rcl_ret_t rclc_executor_add_parameter_server(
   rclc_executor_t * executor,
   rclc_parameter_server_t * parameter_server,
   ModifiedParameter_Callback on_modification);
+
+/**
+ *  Adds a RCLC parameter to a server
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | Yes
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[in] type type of the parameter
+ * \return `RCL_RET_OK` if success
+ */
 
 rcl_ret_t
 rclc_add_parameter(
@@ -129,11 +219,45 @@ rclc_add_parameter(
   const char * parameter_name,
   rclc_parameter_type_t type);
 
+/**
+ *  Sets the value of an existing a RCLC parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[in] varidic value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
+
 rcl_ret_t
 rclc_parameter_set(
   rclc_parameter_server_t * parameter_server,
   const char * parameter_name,
   ...);
+
+/**
+ *  Sets the value of an existing a RCLC bool parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[in] value value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
 
 rcl_ret_t
 rclc_parameter_set_bool(
@@ -141,11 +265,45 @@ rclc_parameter_set_bool(
   const char * parameter_name,
   bool value);
 
+/**
+ *  Sets the value of an existing a RCLC integer parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[in] value value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
+
 rcl_ret_t
 rclc_parameter_set_int(
   rclc_parameter_server_t * parameter_server,
   const char * parameter_name,
   int64_t value);
+
+/**
+ *  Sets the value of an existing a RCLC double parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[in] value value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
 
 rcl_ret_t
 rclc_parameter_set_double(
@@ -153,11 +311,45 @@ rclc_parameter_set_double(
   const char * parameter_name,
   double value);
 
+/**
+ *  Get the value of an existing a RCLC parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[inout] value returns value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
+
 rcl_ret_t
 rclc_parameter_get(
   rclc_parameter_server_t * parameter_server,
   const char * parameter_name,
   void * value);
+
+/**
+ *  Get the value of an existing a RCLC bool parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[inout] value returns value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
 
 rcl_ret_t
 rclc_parameter_get_bool(
@@ -165,11 +357,45 @@ rclc_parameter_get_bool(
   const char * parameter_name,
   bool * output);
 
+/**
+ *  Get the value of an existing a RCLC integer parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[inout] value returns value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
+
 rcl_ret_t
 rclc_parameter_get_int(
   rclc_parameter_server_t * parameter_server,
   const char * parameter_name,
   int * output);
+
+/**
+ *  Get the value of an existing a RCLC double parameter
+ *
+ *  * <hr>
+ * Attribute          | Adherence
+ * ------------------ | -------------
+ * Allocates Memory   | No
+ * Thread-Safe        | No
+ * Uses Atomics       | No
+ * Lock-Free          | No
+ *
+ * \param[in] parameter_server preallocated rclc_parameter_server_t
+ * \param[in] parameter_name name of the parameter
+ * \param[inout] value returns value of the parameter
+ * \return `RCL_RET_OK` if success
+ */
 
 rcl_ret_t
 rclc_parameter_get_double(
