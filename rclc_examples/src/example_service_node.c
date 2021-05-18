8053cab8253906181bd172939a657f4ea672500f
Jan Staschulat <jan.staschulat@de.bosch.com>
2021-05-18 14:07:24 +0200

    replaced github.com/micro-ROS by github.com/ros2
    Signed-off-by: Jan Staschulat <jan.staschulat@de.bosch.com>

--- rclc_examples/src/example_service_node.c
+++ rclc_examples/src/example_service_node.c
@@ -1,5 +1,5 @@
 // Copyright (c) 2020 - for information on the respective copyright owner
-// see the NOTICE file and/or the repository https://github.com/micro-ROS/rclc.
+// see the NOTICE file and/or the repository https://github.com/ros2/rclc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
