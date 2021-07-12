f5c3a03da5033bd2135b469b3fb46bfe10604224
Pablo Garrido <pablogs9@gmail.com>
2021-07-12 14:33:37 +0200

    Tests
    Signed-off-by: Your Name <you@example.com>

--- rclc/test/rclc/test_service.cpp
+++ rclc/test/rclc/test_service.cpp
@@ -124,3 +124,62 @@ TEST(Test, rclc_service_init_best_effort) {
   rc = rclc_support_fini(&support);
   EXPECT_EQ(RCL_RET_OK, rc);
 }
+
+TEST(Test, rclc_service_init_qos) {
+  rclc_support_t support;
+  rcl_ret_t rc;
+
+  // preliminary setup
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  rc = rclc_support_init(&support, 0, nullptr, &allocator);
+  const char * my_name = "test_service_node_be";
+  const char * my_namespace = "";
+  const char * topic_name = "add_two_ints";
+  const char * expected_topic_name = "/add_two_ints";
+  rcl_node_t node = rcl_get_zero_initialized_node();
+  rc = rclc_node_init_default(&node, my_name, my_namespace, &support);
+
+  // test with valid arguments
+  rcl_service_t service = rcl_get_zero_initialized_service();
+  const rmw_qos_profile_t qos_profile = rmw_qos_profile_default;
+  const rosidl_service_type_support_t * type_support =
+    ROSIDL_GET_SRV_TYPE_SUPPORT(test_msgs, srv, BasicTypes);
+  rc = rclc_service_init(&service, &node, type_support, topic_name, &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_OK, rc);
+
+  // check for qos-option best effort
+  const rcl_service_options_t * srv_options = rcl_service_get_options(&service);
+  EXPECT_EQ(srv_options->qos.reliability, rmw_qos_profile_default.reliability);
+
+  // check service topic name
+  EXPECT_EQ(strcmp(rcl_service_get_service_name(&service), expected_topic_name), 0);
+
+  // check that valid service is valid
+  EXPECT_TRUE(rcl_service_is_valid(&service));
+  rcl_reset_error();
+
+  // tests with invalid arguments
+  rc = rclc_service_init_default(nullptr, &node, type_support, topic_name, &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_service_init_default(&service, nullptr, type_support, topic_name, &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_service_init_default(&service, &node, nullptr, topic_name, &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_service_init_default(&service, &node, type_support, nullptr, &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_service_init_default(&service, &node, type_support, topic_name, nullptr);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+
+  // clean up
+  rc = rcl_service_fini(&service, &node);
+  EXPECT_EQ(RCL_RET_OK, rc);
+  rc = rcl_node_fini(&node);
+  EXPECT_EQ(RCL_RET_OK, rc);
+  rc = rclc_support_fini(&support);
+  EXPECT_EQ(RCL_RET_OK, rc);
+}
--- rclc/test/rclc/test_subscription.cpp
+++ rclc/test/rclc/test_subscription.cpp
@@ -103,3 +103,51 @@ TEST(Test, rclc_subscription_init_best_effort) {
   rc = rclc_support_fini(&support);
   EXPECT_EQ(RCL_RET_OK, rc);
 }
+
+TEST(Test, rclc_subscription_init_qos) {
+  rclc_support_t support;
+  rcl_ret_t rc;
+
+  // preliminary setup
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  rc = rclc_support_init(&support, 0, nullptr, &allocator);
+  const char * my_name = "test_name";
+  const char * my_namespace = "test_namespace";
+  rcl_node_t node = rcl_get_zero_initialized_node();
+  rc = rclc_node_init_default(&node, my_name, my_namespace, &support);
+
+  // test with valid arguments
+  rcl_subscription_t subscription = rcl_get_zero_initialized_subscription();
+  const rmw_qos_profile_t qos_profile = rmw_qos_profile_default;
+  const rosidl_message_type_support_t * type_support =
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32);
+  rc = rclc_subscription_init(&subscription, &node, type_support, "topic1", &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_OK, rc);
+  const rcl_subscription_options_t * sub_options = rcl_subscription_get_options(&subscription);
+  EXPECT_EQ(sub_options->qos.reliability, rmw_qos_profile_default.reliability);
+
+  // tests with invalid arguments
+  rc = rclc_subscription_init_best_effort(nullptr, &node, type_support, "topic1", &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_subscription_init_best_effort(&subscription, nullptr, type_support, "topic1", &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_subscription_init_best_effort(&subscription, &node, nullptr, "topic1", &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_subscription_init_best_effort(&subscription, &node, type_support, nullptr, &rmw_qos_profile_default);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+  rc = rclc_subscription_init_best_effort(&subscription, &node, type_support, "topic1", nullptr);
+  EXPECT_EQ(RCL_RET_INVALID_ARGUMENT, rc);
+  rcutils_reset_error();
+
+  // clean up
+  rc = rcl_subscription_fini(&subscription, &node);
+  EXPECT_EQ(RCL_RET_OK, rc);
+  rc = rcl_node_fini(&node);
+  EXPECT_EQ(RCL_RET_OK, rc);
+  rc = rclc_support_fini(&support);
+  EXPECT_EQ(RCL_RET_OK, rc);
+}
