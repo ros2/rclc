004ab3a76f94b9d0b927b43bba06598d9355e056
Jan Staschulat <jan.staschulat@de.bosch.com>
2021-07-28 11:12:23 +0200

    revert dummy changes 2
    Signed-off-by: Jan Staschulat <jan.staschulat@de.bosch.com>

--- rclc/src/rclc/executor.c
+++ rclc/src/rclc/executor.c
@@ -106,7 +106,6 @@ rclc_executor_init(
   }
 
   rcl_ret_t ret = RCL_RET_OK;
-
   (*executor) = rclc_executor_get_zero_initialized_executor();
   executor->context = context;
   executor->max_handles = number_of_handles;
