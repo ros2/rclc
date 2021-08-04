8ca2a50773a1c71bdb7562154edf21c17cc71ce0
Jan Staschulat <jan.staschulat@de.bosch.com>
2021-08-04 16:49:48 +0200

    example_pingpong multiple files
    Signed-off-by: Jan Staschulat <jan.staschulat@de.bosch.com>

--- rclc_examples/src/example_pingpong.c
+++ rclc_examples/src/example_pingpong.c
@@ -1,7 +1,22 @@
+// Copyright (c) 2020 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository https://github.com/ros2/rclc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #include <stdio.h>
 #include <std_msgs/msg/string.h>
 #include <rclc/executor.h>
 #include <rclc/rclc.h>
+#include "example_pingpong_helper.h"
 
 
 // these data structures for the publisher and subscriber are global, so that
@@ -42,12 +57,15 @@ void ping_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 
 void pong_subscription_callback(const void * msgin)
 {
+   pong_subscription_callback_on_update(msgin);
+  /*
   const std_msgs__msg__String * msg = (const std_msgs__msg__String *)msgin;
   if (msg == NULL) {
     printf("Callback: msg NULL\n");
   } else {
     printf("Callback: I heard: %s\n", msg->data.data);
   }
+  */
 }
 
 
